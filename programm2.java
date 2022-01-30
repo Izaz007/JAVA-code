17438009fae786ade2d98af37879e89aea537203
Izaz007 <98402768+Izaz007@users.noreply.github.com>
2022-01-30 17:50:56 +0530

    Add files via upload

---
+++ programm2.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class programm2 {
+  public static void main(String[] args) {
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+    capitalCities.put("England", "London");
+    capitalCities.put("india", "delhi");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("Germany", "Berlin");
+    capitalCities.put("USA", "Washington DC");
+    System.out.println(capitalCities); 
+  }
+}
